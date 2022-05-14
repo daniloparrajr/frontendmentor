c22691d27c6bec62fc34a0b15bfc558df616611b
Danilo Parra Jr <daniloparrajr.work@gmail.com>
2022-05-14 13:09:49 +0800

    feat: add intro section with navigation

--- 08-intro-section-with-dropdown-navigation/src/js/main.js
+++ 08-intro-section-with-dropdown-navigation/src/js/main.js
@@ -1,6 +1,22 @@
-import { helloWorld } from './modules/helpers';
 import '../scss/main.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
-    helloWorld();
+	const navMenuOpen = document.querySelector('.nav-menu-open');
+	const navMenuClose = document.querySelector('.nav-menu-close');
+	const submenuToggles = document.querySelectorAll('.has-submenu > .menu-link');
+
+	navMenuOpen.addEventListener('click', () => {
+		document.body.classList.add('nav-is-active');
+	});
+
+	navMenuClose.addEventListener('click', () => {
+		document.body.classList.remove('nav-is-active');
+	});
+
+	submenuToggles.forEach((submenuToggle) => {
+		submenuToggle.addEventListener('click', (e) => {
+			e.preventDefault();
+			submenuToggle.nextElementSibling.classList.toggle('is-active');
+		});
+	});
 });
