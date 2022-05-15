d50322260b9a60a35a16b6f0e58ad694002ae7c4
Danilo Parra Jr <daniloparrajr.work@gmail.com>
2022-05-15 15:52:51 +0800

    fix: add animations to intro section with dropdown nav challenge

--- 08-intro-section-with-dropdown-navigation/src/js/main.js
+++ 08-intro-section-with-dropdown-navigation/src/js/main.js
@@ -1,17 +1,26 @@
 import '../scss/main.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
+	const body = document.body;
+	const nav = document.querySelector('.nav');
 	const navMenuOpen = document.querySelector('.nav-menu-open');
 	const navMenuClose = document.querySelector('.nav-menu-close');
 	const submenuToggles = document.querySelectorAll('.has-submenu > .menu-link');
 
 	navMenuOpen.addEventListener('click', () => {
-		document.body.classList.add('nav-is-active');
+		body.classList.add('nav-is-open');
+		body.classList.remove('nav-is-close');
 	});
 
 	navMenuClose.addEventListener('click', () => {
-		document.body.classList.remove('nav-is-active');
+		body.classList.replace('nav-is-open', 'nav-is-closing');
 	});
+
+	nav.addEventListener('animationend', () => {
+		if (body.classList.contains('nav-is-closing')) {
+			body.classList.replace('nav-is-closing', 'nav-is-close');
+		}
+	}, false);
 
 	submenuToggles.forEach((submenuToggle) => {
 		submenuToggle.addEventListener('click', (e) => {
